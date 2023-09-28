8070ff80060743390a5b32ce5c7b05ef31ac5544
Devon Bull <devjbull@gmail.com>
2023-09-28 18:22:47 -0400

    setup Google analytics

---
+++ app/gtag.ts
@@ -0,0 +1,21 @@
+// Google Analytics Tracking ID
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID
+
+// export const pageview = (url: string): void => {
+//     window.gtag('config', GA_TRACKING_ID, {
+//         page_path: url,
+//     })
+// }
+
+export const event = ({ action, category, label, value }: {
+    action: string;
+    category: string;
+    label: string;
+    value: number;
+}): void => {
+    window.gtag("event", action, {
+        event_category: category,
+        event_label: label,
+        value: value,
+    });
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
 import { ThemeProvider } from '@/components/theme-provider'
 import { SiteHeader } from "@/components/site-header";
 import {TailwindIndicator} from "@/components/tailwind-indicator";
+import GoogleAnalytics from "@/app/google-analytics";
 
 config.autoAddCss = false
 
@@ -19,7 +20,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-
+      <GoogleAnalytics />
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
